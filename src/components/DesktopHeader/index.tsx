8966679c74e3b9161387ccbca142d1aa24dd82ac
Wander Souza <wanderrodrigues_@outlook.com>
2021-03-29 17:33:05 -0300

    desktop-header

--- src/components/DesktopHeader/index.tsx
+++ src/components/DesktopHeader/index.tsx
@@ -1,9 +1,53 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { SearchInput } from '../MobileHeader/styles';
+
+import { 
+  Container,
+  Wrapper,
+  LinkedInIcon,
+  SearchInput,
+  HomeIcon,
+  NotificationIcon,
+  CaretDownIcon,
+} from './styles';
 
 const DesktopHeader: React.FC = () => {
-  return <div />;
+  return(
+    <Container>
+      <Wrapper>
+
+        <div className="left">
+          <LinkedInIcon />
+          <SearchInput placeholder="Pesquisar"/>
+        </div>
+
+        <div className="right">
+          <nav>
+            <button className="active">
+              <HomeIcon />
+              <span>
+                Inicio
+              </span>
+            </button>
+            <button>
+              <NotificationIcon />
+              <span>
+                Notificações
+              </span>
+            </button>
+            <button>
+            <ProfileCircle src="https://avatars.githubusercontent.com/u/48796830?v=4" />
+              <span>
+                Eu <CaretDownIcon />
+              </span>
+            </button>
+          </nav>
+        </div>
+
+      </Wrapper>
+    </Container>
+  );
 }
 
 export default DesktopHeader;
--- src/components/DesktopHeader/styles.ts
+++ src/components/DesktopHeader/styles.ts
@@ -1,5 +1,18 @@
 import styled from 'styled-components';
+import {GrLinkedin} from 'react-icons/gr';
+import {AiFillHome, AiOutlineBell, AiFillCaretDown} from 'react-icons/ai';
 
-export const Container = styled.div`
-  
-`;
+
+export const Container = styled.div``;
+
+export const Wrapper = styled.div``;
+
+export const LinkedInIcon = styled(GrLinkedin);
+
+export const SearchInput = styled.div``;
+
+export const HomeIcon = styled(AiFillHome);
+
+export const NotificationIcon = styled(AiOutlineBell);
+
+export const CaretDownIcon = styled(AiFillCaretDown);
